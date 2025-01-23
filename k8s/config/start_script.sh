e641a90e9c06620d015cf1bb888550a015b42da2
Nikša Brala <nikbrala@gmail.com>
2025-01-23 21:15:23 +0100

    Path in start_script.sh fixed

--- k8s/config/start_script.sh
+++ k8s/config/start_script.sh
@@ -4,9 +4,9 @@ kubectl delete -f temperature-config.yaml
 
 kubectl logs humidity-microservice-6789f8b9cb-22q6n
 
-kubectl create configmap readings --from-file=C:\F__A__X\PRVI\RASSUS\lab_3\niksa_brala\k8s\config\readings.csv
+kubectl create configmap readings --from-file=C:\F___A___X\PRVI\RASSUS\lab_3\niksa_brala\k8s\config\readings.csv
 
-kubectl create configmap application-properties --from-file=C:\F__A__X\PRVI\RASSUS\lab_3\niksa_brala\k8s\config\application.properties
+kubectl create configmap application-properties --from-file=C:\F___A___X\PRVI\RASSUS\lab_3\niksa_brala\k8s\config\application.properties
 
 kubectl get pods
 kubectl get gateways -A
