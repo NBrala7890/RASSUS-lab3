c170e3e66373e73491f37cc15c2cd7e344b7f5ac
Nikša Brala <nikbrala@gmail.com>
2025-01-23 21:10:16 +0100

    Files organised and deployment-instructions.txt completed

---
+++ k8s/config/start_script.sh
@@ -0,0 +1,13 @@
+kubectl apply -f temperature-deployment.yaml
+
+kubectl delete -f temperature-config.yaml
+
+kubectl logs humidity-microservice-6789f8b9cb-22q6n
+
+kubectl create configmap readings --from-file=C:\F__A__X\PRVI\RASSUS\lab_3\niksa_brala\k8s\config\readings.csv
+
+kubectl create configmap application-properties --from-file=C:\F__A__X\PRVI\RASSUS\lab_3\niksa_brala\k8s\config\application.properties
+
+kubectl get pods
+kubectl get gateways -A
+kubectl get configmaps
